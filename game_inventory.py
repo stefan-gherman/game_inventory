dbcb2edc510077d5dd45404e0052edd6ead15084
Stefan Gherman <stefangheman@gmail.com>
2019-10-27 16:00:51 +0200

    display function with basic display

--- game_inventory.py
+++ game_inventory.py
@@ -1,15 +1,31 @@
 # game inventory functions file
 
-def display_inventory(inventory):
+def display_inventory(inventory):   # Step 1
     for item, value in inventory.items():
         print("{}:{}".format(item, value))
 
-def add_to_inventory(inventory, added_items):
+
+def add_to_inventory(inventory, added_items): # Step 2
     for item in added_items:
         if item in inventory.keys():
             inventory[item] += 1
         else:
             inventory[item] = 1     
+
+
+def print_table(inventory, order = ""):
+
+
+    displayable_dict = sorted(inventory.items(), key = lambda item,value: value)
+
+    
+    print(type(displayable_dict))
+
+    print("-----------------")
+    print("item name | count")
+    print("-----------------")
+    print(displayable_dict)
+
 
 #tests
 INV = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}        
@@ -19,3 +35,5 @@ print("\n")
 add_to_inventory(INV, dragon_loot)
 display_inventory(INV)
 
+print_table(INV)
+  
