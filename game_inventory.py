b202c33d8ccd6962ac9ebb1f27b28c03f60fc9eb
Stefan Gherman <stefangheman@gmail.com>
2019-10-27 06:02:00 +0200

    Added ability to accquire loot

--- game_inventory.py
+++ game_inventory.py
@@ -1,12 +1,21 @@
 # game inventory functions file
 
 def display_inventory(inventory):
-
     for item, value in inventory.items():
         print("{}:{}".format(item, value))
 
-
+def add_to_inventory(inventory, added_items):
+    for item in added_items:
+        if item in inventory.keys():
+            inventory[item] += 1
+        else:
+            inventory[item] = 1     
 
 #tests
-inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}        
-display_inventory(inv)
+INV = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}        
+display_inventory(INV)
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+print("\n")
+add_to_inventory(INV, dragon_loot)
+display_inventory(INV)
+
